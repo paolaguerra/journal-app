fea231cfef37521ba714f3e6d33360c90361e68c
paolaguerra <paolaguerraph@gmail.com>
2023-03-03 11:42:29 -0500

    Organizacion de carpetas

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './styles.css';
 import reportWebVitals from './reportWebVitals';
 import { JournalApp } from './JournalApp';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <JournalApp />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
