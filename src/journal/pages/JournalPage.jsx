e4c1a394770b2e5282b7cce424815ecce45ccd35
paolaguerra <paolaguerraph@gmail.com>
2023-03-10 14:44:03 -0500

    Galeria y boton de agregar evento

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,8 @@
+import { AddOutlined } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 import { JournalLayaout } from "../layout/JournalLayaout";
-import { NoteView } from "../views/NoteView";
-// import { NothingSelectedView } from "../views/NothingSelectedView";
+// import { NoteView } from "../views/NoteView";
+import { NothingSelectedView } from "../views/NothingSelectedView";
 
 
 export const JournalPage = () => {
@@ -10,8 +12,22 @@ export const JournalPage = () => {
        
        {/* <Typography>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Typography> */}
 
-        {/* <NothingSelectedView /> */}
-        <NoteView />
+        <NothingSelectedView />
+        {/* <NoteView /> */}
+
+        <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+        >
+          <AddOutlined sx={{ fontsize: 30 }} />
+        </IconButton>
 
       </JournalLayaout>
     </>
