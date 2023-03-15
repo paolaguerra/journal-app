3844ac500a070bd46c19137c586b9807cd0ba612
paolaguerra <paolaguerraph@gmail.com>
2023-03-15 12:44:22 -0400

    Instalación de Redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles.css';
-import reportWebVitals from './reportWebVitals';
-import { JournalApp } from './JournalApp';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+import reportWebVitals from "./reportWebVitals";
+import { JournalApp } from "./JournalApp";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
